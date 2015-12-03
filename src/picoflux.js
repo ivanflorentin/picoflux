68e0e7ca1910ff10f92b534f44e74cf83073ae8d
ivan <ivan@inergetica.com>
2015-12-02 22:19:40 -0300

    new version, stores decoupled from components, multiple stores

--- src/picoflux.js
+++ src/picoflux.js
@@ -8,40 +8,121 @@ class Flux {
 
     constructor() {
         this.actions = {};
-        this.observers = [];
+        this.events = {};
+	this.stores = {};
     }
-    addAction(name) {
-        this.actions[name] = [];
+
+    /*
+     Add an action that will be propagated to a listener, 
+     often by user interaction
+     */
+    addAction(actionName) {
+	if (actionName in this.actions){
+	    console.error('action already exists!', actionName);
+	}
+	else {
+            this.actions[actionName] = [];
+	}
     }
-    addListener(name, func) {
-        if (name in this.actions) {
-            this.actions[name].push(func);
+
+    /*
+     Add a listener function to an action, often to manipulate data in the 
+     store
+     */
+    addListener(actionName, func) {
+        if (actionName in this.actions) {
+            this.actions[actionName].push(func);
         } else {
-            console.log('No action with that name:', name);
-        }
-    }
-    action(name, payload) {
-        if (name in this.actions) {
-            if (this.actions[name].length > 0) {
-                for (var func of this.actions[name]) {
-                    func(payload);
-                }
-            } else {
-                console.log('No listeners for action: ', name);
-            }
-        } else {
-            console.log('Action not found: ', name, 'Payload: ', payload);
+            console.error('No action with that name:', actionName);
         }
     }
 
-    attach(func) {
-        this.observers.push(func);
-    }
-    notify() {
-        for (var observer of this.observers) {
-            observer();
+    /*
+     Excecute the action, call the listeners with the pyload
+     */
+    action(actionName, payload) {
+        if (actionName in this.actions) {
+            if (this.actions[actionName].length > 0) {
+                for (var func of this.actions[actionName]) {
+                    func(payload);
+                }
+            } else {
+                console.error('No listeners for action: ', actionName);
+            }
+        } else {
+            console.error('Action not found: ', actionName, 'Payload: ',
+			  payload);
         }
     }
+
+    /*
+     Add a flux event
+     */
+    addEvent(fluxEvent){
+	if (fluxEvent in this.events){
+	    console.error('event already exists!', fluxEvent);
+	}
+	else {
+	    this.events[fluxEvent] = [];
+	}
+    }
+
+    /*
+     Susccribe to a flux Event, generally a data change
+     */
+    suscribe(fluxEvent, func) {
+	if (fluxEvent in this.events){
+            this.events[fluxEvent].push(func);	    
+	}
+	else {
+	    console.error('Unable to attach event. Event does not exists: ',
+			fluxEvent);
+	}
+    }
+    
+    /*
+     Notify the siscribers of a flux event, generally a dataChange for 
+     re render
+     */
+    notify(fluxEvent) {
+	if (fluxEvent in this.events){
+            for (var observer of this.events[fluxEvent]) {
+		observer();
+            }
+	}
+	else{
+	    console.log('Unable to notify unexisting event: ', fluxEvent);
+	}
+    }
+    
+    /*
+     Register a datastore with a handler function for data retreival 
+     */
+    registerStore(rootName, func){
+	if (rootName in this.stores){
+	    console.log('Store already registered');
+	}
+	else {
+	    this.stores[rootName] = func;
+	}
+    }
+    
+    /*
+     Get the data from a store.
+     param: dataRoute,  a list representing the route of the data required. 
+     The first element represents the dataStore name.
+     */
+    getData(dataRoute){
+	var data = '';
+	if(dataRoute[0] in this.stores){
+	    let p = this.stores[dataRoute[0]](dataRoute);
+	    return p;
+	}
+	else{
+	    return new Promise.reject('DataPath not found:', dataRoute);
+	}
+    }
 }
+
 export default Flux;
 
