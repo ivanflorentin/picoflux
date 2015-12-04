ec1145f88d34c2045e7d53fe2219927c8bd23018
demian <demian@sinergetica.com>
2015-12-04 18:05:07 -0300

    promises added to picoflux api

--- picoflux.js
+++ picoflux.js
@@ -6,205 +6,253 @@
 "use strict";
 
 var _createClass = (function () {
-    function defineProperties(target, props) {
-        for (var i = 0; i < props.length; i++) {
-            var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);
-        }
-    }return function (Constructor, protoProps, staticProps) {
-        if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;
-    };
+	function defineProperties(target, props) {
+		for (var i = 0; i < props.length; i++) {
+			var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);
+		}
+	}return function (Constructor, protoProps, staticProps) {
+		if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;
+	};
 })();
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+	value: true
 });
 
 function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-        throw new TypeError("Cannot call a class as a function");
-    }
+	if (!(instance instanceof Constructor)) {
+		throw new TypeError("Cannot call a class as a function");
+	}
 }
 
 var Flux = (function () {
-    function Flux() {
-        _classCallCheck(this, Flux);
+	function Flux() {
+		_classCallCheck(this, Flux);
 
-        this.actions = {};
-        this.events = {};
-        this.stores = {};
-    }
+		this.actions = {};
+		this.events = {};
+		this.stores = {};
+	}
 
-    /*
-     Add an action that will be propagated to a listener, 
-     often by user interaction
-     */
+	/*
+  Add an action that will be propagated to a listener, 
+  often by user interaction
+  */
 
-    _createClass(Flux, [{
-        key: 'addAction',
-        value: function addAction(actionName) {
-            if (actionName in this.actions) {
-                console.error('action already exists!', actionName);
-            } else {
-                this.actions[actionName] = [];
-            }
-        }
+	_createClass(Flux, [{
+		key: 'addAction',
+		value: function addAction(actionName) {
+			var _this = this;
 
-        /*
-         Add a listener function to an action, often to manipulate data in the 
-         store
-         */
+			return new Promise(function (resolve, reject) {
+				if (actionName in _this.actions) {
+					var msg = 'Action {Action} already exists!'.replace('{Action}', actionName);
+					reject(new Error(msg));
+				} else {
+					_this.actions[actionName] = [];
+					resolve(actionName);
+				}
+			});
+		}
 
-    }, {
-        key: 'addListener',
-        value: function addListener(actionName, func) {
-            if (actionName in this.actions) {
-                this.actions[actionName].push(func);
-            } else {
-                console.error('No action with that name:', actionName);
-            }
-        }
+		/*
+   Add a listener function to an action, often to manipulate data in the 
+   store
+   */
 
-        /*
-         Excecute the action, call the listeners with the pyload
-         */
+	}, {
+		key: 'addListener',
+		value: function addListener(actionName, func) {
+			var _this2 = this;
 
-    }, {
-        key: 'action',
-        value: function action(actionName, payload) {
-            if (actionName in this.actions) {
-                if (this.actions[actionName].length > 0) {
-                    var _iteratorNormalCompletion = true;
-                    var _didIteratorError = false;
-                    var _iteratorError = undefined;
+			return new Promise(function (resolve, reject) {
+				if (actionName in _this2.actions) {
+					_this2.actions[actionName].push(func);
+					resolve(actionName);
+				} else {
+					var msg = 'No Action with the name {Action} found!'.replace('{Action}', actionName);
+					reject(new Error(msg));
+				}
+			});
+		}
 
-                    try {
-                        for (var _iterator = this.actions[actionName][Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-                            var func = _step.value;
+		/*
+   Excecute the action, call the listeners with the pyload
+   */
 
-                            func(payload);
-                        }
-                    } catch (err) {
-                        _didIteratorError = true;
-                        _iteratorError = err;
-                    } finally {
-                        try {
-                            if (!_iteratorNormalCompletion && _iterator.return) {
-                                _iterator.return();
-                            }
-                        } finally {
-                            if (_didIteratorError) {
-                                throw _iteratorError;
-                            }
-                        }
-                    }
-                } else {
-                    console.error('No listeners for action: ', actionName);
-                }
-            } else {
-                console.error('Action not found: ', actionName, 'Payload: ', payload);
-            }
-        }
+	}, {
+		key: 'action',
+		value: function action(actionName, payload) {
+			var _this3 = this;
 
-        /*
-         Add a flux event
-         */
+			return new Promise(function (resolve, reject) {
+				if (actionName in _this3.actions) {
+					if (_this3.actions[actionName].length > 0) {
+						var _iteratorNormalCompletion = true;
+						var _didIteratorError = false;
+						var _iteratorError = undefined;
 
-    }, {
-        key: 'addEvent',
-        value: function addEvent(fluxEvent) {
-            if (fluxEvent in this.events) {
-                console.error('event already exists!', fluxEvent);
-            } else {
-                this.events[fluxEvent] = [];
-            }
-        }
+						try {
+							for (var _iterator = _this3.actions[actionName][Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+								var func = _step.value;
 
-        /*
-         Susccribe to a flux Event, generally a data change
-         */
+								func(payload);
+								resolve(actionName);
+							}
+						} catch (err) {
+							_didIteratorError = true;
+							_iteratorError = err;
+						} finally {
+							try {
+								if (!_iteratorNormalCompletion && _iterator.return) {
+									_iterator.return();
+								}
+							} finally {
+								if (_didIteratorError) {
+									throw _iteratorError;
+								}
+							}
+						}
+					} else {
+						var msg = "No listeners for action: " + actionName;
+						reject(new Error(msg));
+					}
+				} else {
+					var msg = 'No Action with the name {Action} found!'.replace('{Action}', actionName);
+					reject(new Error(msg));
+				}
+			});
+		}
 
-    }, {
-        key: 'suscribe',
-        value: function suscribe(fluxEvent, func) {
-            if (fluxEvent in this.events) {
-                this.events[fluxEvent].push(func);
-            } else {
-                console.error('Unable to attach event. Event does not exists: ', fluxEvent);
-            }
-        }
+		/*
+   Add a flux event
+   */
 
-        /*
-         Notify the siscribers of a flux event, generally a dataChange for 
-         re render
-         */
+	}, {
+		key: 'addEvent',
+		value: function addEvent(fluxEvent) {
+			var _this4 = this;
 
-    }, {
-        key: 'notify',
-        value: function notify(fluxEvent) {
-            if (fluxEvent in this.events) {
-                var _iteratorNormalCompletion2 = true;
-                var _didIteratorError2 = false;
-                var _iteratorError2 = undefined;
+			return new Promise(function (resolve, reject) {
+				if (fluxEvent in _this4.events) {
+					var msg = 'event {FluxEvent} already exists!'.replace("{FluxEvent}", fluxEvent);
+					reject(new Error(msg));
+				} else {
+					_this4.events[fluxEvent] = [];
+					resolve(fluxEvent);
+				}
+			});
+		}
 
-                try {
-                    for (var _iterator2 = this.events[fluxEvent][Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-                        var observer = _step2.value;
+		/*
+   Susccribe to a flux Event, generally a data change
+   */
 
-                        observer();
-                    }
-                } catch (err) {
-                    _didIteratorError2 = true;
-                    _iteratorError2 = err;
-                } finally {
-                    try {
-                        if (!_iteratorNormalCompletion2 && _iterator2.return) {
-                            _iterator2.return();
-                        }
-                    } finally {
-                        if (_didIteratorError2) {
-                            throw _iteratorError2;
-                        }
-                    }
-                }
-            } else {
-                console.log('Unable to notify unexisting event: ', fluxEvent);
-            }
-        }
+	}, {
+		key: 'suscribe',
+		value: function suscribe(fluxEvent, func) {
+			var _this5 = this;
 
-        /*
-         Register a datastore with a handler function for data retreival 
-         */
+			return new Promise(function (resolve, reject) {
+				if (fluxEvent in _this5.events) {
+					_this5.events[fluxEvent].push(func);
+					resolve(fluxEvent);
+				} else {
+					var msg = 'Unable to attach event. Event {FluxEvent} does not exists.'.replace("{FluxEvent}", fluxEvent);
+					reject(new Error(msg));
+				}
+			});
+		}
 
-    }, {
-        key: 'registerStore',
-        value: function registerStore(rootName, func) {
-            if (rootName in this.stores) {
-                console.log('Store already registered');
-            } else {
-                this.stores[rootName] = func;
-            }
-        }
+		/*
+   Notify the siscribers of a flux event, generally a dataChange for 
+   re render
+   */
 
-        /*
-         Get the data from a store.
-         param: dataRoute,  a list representing the route of the data required. 
-         The first element represents the dataStore name.
-         */
+	}, {
+		key: 'notify',
+		value: function notify(fluxEvent) {
+			var _this6 = this;
 
-    }, {
-        key: 'getData',
-        value: function getData(dataRoute) {
-            var data = '';
-            if (dataRoute[0] in this.stores) {
-                var p = this.stores[dataRoute[0]](dataRoute);
-                return p;
-            } else {
-                return new Promise.reject('DataPath not found:', dataRoute);
-            }
-        }
-    }]);
+			return new Promise(function (resolve, reject) {
+				if (fluxEvent in _this6.events) {
+					var _iteratorNormalCompletion2 = true;
+					var _didIteratorError2 = false;
+					var _iteratorError2 = undefined;
 
-    return Flux;
+					try {
+						for (var _iterator2 = _this6.events[fluxEvent][Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+							var observer = _step2.value;
+
+							observer();
+							resolve(fluxEvent);
+						}
+					} catch (err) {
+						_didIteratorError2 = true;
+						_iteratorError2 = err;
+					} finally {
+						try {
+							if (!_iteratorNormalCompletion2 && _iterator2.return) {
+								_iterator2.return();
+							}
+						} finally {
+							if (_didIteratorError2) {
+								throw _iteratorError2;
+							}
+						}
+					}
+				} else {
+					var msg = 'Unable to notify unexisting event: {FluxEvent}.'.replace("{FluxEvent}", fluxEvent);
+					reject(new Error(msg));
+				}
+			});
+		}
+
+		/*
+   Register a datastore with a handler function for data retreival 
+   */
+
+	}, {
+		key: 'registerStore',
+		value: function registerStore(rootName, func) {
+			var _this7 = this;
+
+			return new Promise(function (resolve, reject) {
+				if (rootName in _this7.stores) {
+					var msg = 'Store {Store} already registered'.replace("{Store}", rootName);
+					reject(new Error(msg));
+				} else {
+					_this7.stores[rootName] = func;
+					resolve(rootName);
+				}
+			});
+		}
+
+		/*
+   Get the data from a store.
+   param: dataRoute,  a list representing the route of the data required. 
+   The first element represents the dataStore name.
+   */
+
+	}, {
+		key: 'getData',
+		value: function getData(dataRoute) {
+			var _this8 = this;
+
+			return new Promise(function (resolve, reject) {
+				var data = '';
+				if (dataRoute[0] in _this8.stores) {
+					var p = _this8.stores[dataRoute[0]](dataRoute);
+					resolve(p);
+				} else {
+					var msg = 'DataPath {DataRoute} not found'.replace("{DataRoute}", dataRoute);
+					reject(new Error(msg));
+				}
+			});
+		}
+	}]);
+
+	return Flux;
 })();
 
 exports.default = Flux;
